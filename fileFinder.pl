fdaed3e4c77a9c401606253147f81681f72facd7
Xiaobing Zhang <xiaobingz@me-desktop-1.nvidia.com>
2016-02-04 21:33:24 -0800

    First workable version

--- fileFinder.pl
+++ fileFinder.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/perl -w
 
-#use feature 'unicode_strings';
 use utf8;
 use strict;
 use Excel::Writer::XLSX;
@@ -13,15 +12,15 @@ my ($directory, $outFile) = @ARGV;
 $directory && $outFile || die ( "$usage" );
 
 #Remove .xls or .xlsx
-#$outFile =~ s/\.xls(x?)//;
+$outFile =~ s/\.xls(x?)//;
 
 my ($sec,$min,$hour,$mday,$mon,$year) = localtime;
 $year += 1900;
 my $padString = sprintf("%04d%02d%02d_%02d%02d%02d", $year, $mon, $mday, $hour, $min, $sec).".xlsx";
 
-#$outFile .= "_".$padString;
+$outFile .= "_".$padString;
 
-#binmode(STDOUT, ":encoding(utf8)");
+binmode(STDOUT, ":encoding(utf8)");
 print "Filename $outFile\n";
 
 sub findFiles{
@@ -37,38 +36,65 @@ sub findFiles{
 
 # Create a new Excel workbook
 my $workbook = Excel::Writer::XLSX->new($outFile);
-my $format = $workbook->add_format();
-$format->set_bold();
-$format->set_bg_color( 'yellow' );
 
+# Create format for first subdirectory line
+my $topFormat = $workbook->add_format();
+$topFormat->set_bold();
+$topFormat->set_bg_color( 'yellow' );
+$topFormat->set_align("left");
+$topFormat->set_font("Times New Roman");
+
+# Create format for normal cell
+my $itemFormat = $workbook->add_format();
+$itemFormat->set_align("left");
+$itemFormat->set_font("Times New Roman");
+
+# Enable verbose output
 my $debug = 1;
+
 for my $subsheet (findFiles($directory)) {
     #Create a saparate worksheet
     my $rowNum = 0;
     my $sheetPath = $directory."/".$subsheet; 
     if(opendir(my $dh, $sheetPath)) {
         closedir $dh;
+        
+        #Covert string to UTF8
+        $subsheet = decode("utf8",$subsheet);
+        print "Sheet: $subsheet\n" if $debug;
 
+        #Create new worksheet
         my $worksheet = $workbook->add_worksheet($subsheet);
         $worksheet->keep_leading_zeros();
-        print "Sheet: $subsheet\n" if $debug;
+        $worksheet->set_column('A:A',100,$itemFormat);
 
         #Search subsheet
         for my $subDir (findFiles($sheetPath)) {
             my @treeOutputs = split /\n/, qx(tree $sheetPath/$subDir);
-            @treeOutputs = grep { !/^(\w|\s)/ } @treeOutputs;
+            my $errorCount = grep { /error opening dir/ } @treeOutputs;
 
-            #We need to highlight this line
+            #Covert string to UTF8
+            $subDir = decode("utf8",$subDir);
             print "$subDir\n" if $debug;
-            $worksheet->write($rowNum, 0, $subDir, $format);
-            $rowNum++;
+            print "ErrorCounts: $errorCount\n" if $debug;
+
+            #We don't need to highlight file node
+            if($errorCount) {
+                $worksheet->write($rowNum++, 0, $subDir, $itemFormat);
+
+                #Clear arrary for file node
+                @treeOutputs = ();
+            } else {
+                $worksheet->write($rowNum++, 0, $subDir, $topFormat);
+
+                #Get arrary content
+                @treeOutputs = grep { !/^\w/ } @treeOutputs;
+            }
 
             for my $subline (@treeOutputs) {
-                #$subline =~ s/\W/ /g;
-                #$subline = encode("utf8", $subline);
+                $subline = decode("utf8", $subline);
                 print "$subline\n" if $debug;
-                $worksheet->write($rowNum, 0, $subline);
-                $rowNum++;
+                $worksheet->write($rowNum++, 0, $subline, $itemFormat);
             }
         }
     }
